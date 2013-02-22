a0f48936948663e81932a7b97e9f269dc5a8ef10
Martin Gruner <martin.gruner@otrs.com>
2013-02-22 17:58:01 +0100

    Improved check: allow  and , allow leading _ to mark internal variables/functions

--- Perl/Critic/Policy/OTRS/RequireCamelCase.pm
+++ Perl/Critic/Policy/OTRS/RequireCamelCase.pm
@@ -23,29 +23,29 @@ my %dispatcher = (
     'PPI::Token::Symbol'      => \&_variable_is_camelcase,
 );
 
-sub applies_to { 
+sub applies_to {
     keys %dispatcher,
 }
 
 sub violates {
     my ( $self, $elem ) = @_;
-    
+
     my $ref = ref $elem;
     my $sub = $dispatcher{$ref};
     return if !$sub;
-    
+
     my $success = $self->$sub( $elem );
-    
+
     return if $success;
     return $self->violation( $DESC, $EXPL, $elem );
 }
 
 sub _is_camelcase {
     my ( $self, $elem ) = @_;
-    
+
     my $words = $elem->find( 'PPI::Token::Word' );
     my $name  = $words->[1];
-    
+
     if ( $elem->isa( 'PPI::Statement::Sub' ) and $name eq 'new' ) {
         return 1;
     }
@@ -55,21 +55,24 @@ sub _is_camelcase {
     elsif ( $elem->isa( 'PPI::Statement::Package' ) and $name =~ m{ Language :: [a-z]{2,3}_ }xms ) {
         return 1;
     }
-    
+
     return 1 if !$name;
-    
-    my $is_camelcase = !( $name !~ m{ \A [A-Z][a-z]* }xms || $name =~ m{ _ }xms );
-    
+
+    my $is_camelcase = !( $name !~ m{ \A _* [A-Z][a-z]* }xms || $name =~ m{ [^_]_ }xms );
+
     return $is_camelcase;
 }
 
 sub _variable_is_camelcase {
     my ( $self, $elem ) = @_;
-    
+
     my $name = "$elem";
-    
-    my $is_camelcase = !( $name !~ m{ \A [\*\@\$\%][A-Z][a-z]* }xms || $name =~ m{ _ }xms );
-    
+
+    return 1 if $name eq '$a';
+    return 1 if $name eq '$b';
+
+    my $is_camelcase = !( $name !~ m{ \A [\*\@\$\%]_*[A-Z][a-z]* }xms || $name =~ m{ [^_]_ }xms );
+
     return $is_camelcase;
 }
 
