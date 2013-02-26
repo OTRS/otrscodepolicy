2adee47a278806d0ad54a87a18e819773b34eb92
Michiel Beijen <michiel.beijen@otrs.com>
2013-02-26 10:42:55 +0100

    Added first version of XMLLint validator

---
+++ TidyAll/Plugin/XMLLint.pm
@@ -0,0 +1,83 @@
+package Code::TidyAll::Plugin::XMLLint;
+BEGIN {
+  $Code::TidyAll::Plugin::XMLLint::VERSION = '0.01';
+}
+use Capture::Tiny qw(capture_merged);
+use Moo;
+extends 'Code::TidyAll::Plugin';
+ 
+sub _build_cmd { 'xmllint' }
+ 
+sub validate_file {
+    my ( $self, $file ) = @_;
+ 
+    my $cmd = sprintf( "%s %s %s", $self->cmd, $self->argv, $file );
+    my $output = capture_merged { system($cmd) };
+    die "$output\n" if $output !~ /is OK\./;
+}
+ 
+1;
+ 
+ 
+ 
+=pod
+ 
+=head1 NAME
+ 
+Code::TidyAll::Plugin::XMLLint - use xmllint with tidyall
+ 
+=head1 VERSION
+ 
+version 0.01
+ 
+=head1 SYNOPSIS
+ 
+   In configuration:
+ 
+   [XMLLint]
+   select = static/**/*.xml
+ 
+=head1 DESCRIPTION
+ 
+Runs xmllint, an XML validator, and dies if any problems are found.
+ 
+=head1 INSTALLATION
+ 
+Install xmllint, then install this module via CPAN. xmllint is probably available 
+from your linux repository: on Red Hat Enterprise Linux or CentOS it is packaged
+in the libxml2 package. On Windows it is installed when you install 
+L<Strawberry Perl|http://strawberryperl.com/>.
+ 
+=head1 CONFIGURATION
+ 
+=over
+ 
+=item argv
+ 
+Arguments to pass to xmllint
+ 
+=item cmd
+ 
+Full path to xmllint
+ 
+=back
+ 
+=head1 SEE ALSO
+ 
+L<Code::TidyAll|Code::TidyAll>
+ 
+=head1 AUTHOR
+ 
+Michiel Beijen <michiel.beijen@otrs.com>
+ 
+=head1 COPYRIGHT AND LICENSE
+ 
+This software is copyright (c) 2013 by Michiel Beijen
+ 
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+ 
+=cut
+ 
+ 
+__END__
