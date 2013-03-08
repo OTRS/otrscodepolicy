000a16e69ec7b2a96f1099243aafec8dfe34cfbd
Martin Gruner <martin.gruner@otrs.com>
2013-03-08 17:22:02 +0100

    Added another XML check.

---
+++ TidyAll/Plugin/OTRS/XMLCheckWithOurParsers.pm
@@ -0,0 +1,32 @@
+package TidyAll::Plugin::OTRS::XMLCheckWithOurParsers;
+
+use strict;
+use warnings;
+
+BEGIN {
+  $TidyAll::Plugin::OTRS::PerlTidy::XMLCheckWithOurParsers::VERSION = '0.1';
+}
+use Moo;
+extends 'Code::TidyAll::Plugin';
+
+use XML::Parser;
+
+sub validate_source {
+    my ( $Self, $Code ) = @_;
+
+    my $Parser = XML::Parser->new();
+    if ( !eval { $Parser->parse( $Code ) } ) {
+        die "ERROR: XMLCheckWithOurParsers() - XML::Parser produced errors!\n";
+    }
+
+    # XML::Parser::Lite may not be installed, only check if present.
+    if ( eval 'require XML::Parser::Lite' ) { ## no critic
+        my $ParserLite =  XML::Parser::Lite->new();
+        eval { $ParserLite->parse( $Code ) };
+        if ( $@ ) {
+            die "ERROR: XMLCheckWithOurParsers() - XML::Parser::Lite produced errors!\n";
+        }
+    }
+}
+
+1;
