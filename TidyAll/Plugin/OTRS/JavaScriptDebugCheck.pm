5b07b8c957a5978422d929fc1c795bbd67477ff8
Martin Gruner <martin.gruner@otrs.com>
2013-03-12 15:08:36 +0100

    Added JS test.

---
+++ TidyAll/Plugin/OTRS/JavaScriptDebugCheck.pm
@@ -0,0 +1,28 @@
+package TidyAll::Plugin::OTRS::JavaScriptDebugCheck;
+
+use strict;
+use warnings;
+
+BEGIN {
+  $TidyAll::Plugin::OTRS::JavaScriptDebugCheck::VERSION = '0.1';
+}
+use Moo;
+extends 'Code::TidyAll::Plugin';
+
+sub validate_source {
+    my ( $Self, $Code ) = @_;
+
+    my $Error;
+    my $Counter;
+
+    for my $Line ( split(/\n/, $Code) ) {
+        $Counter++;
+        if ( $Line =~ m{ console\.log\( }xms ) {
+            $Error .= "ERROR: JavaScriptDebugCheck() found a console.log() statement in line( $Counter ): $Line\n";
+            $Error .= "This will break IE and Opera. Please remove it from your code.\n";
+        }
+    }
+    die $Error if ($Error);
+}
+
+1;
