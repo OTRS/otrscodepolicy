5f66a878f0062038accb7bbcdd318ebb1a623f43
Martin Gruner <martin.gruner@otrs.com>
2013-08-19 14:27:16 +0200

    Added check for package documentation.

---
+++ TidyAll/Plugin/OTRS/SOPM/DocumentationPresent.pm
@@ -0,0 +1,20 @@
+package TidyAll::Plugin::OTRS::SOPM::DocumentationPresent;
+
+use strict;
+use warnings;
+
+use base qw(TidyAll::Plugin::OTRS::Base);
+
+sub validate_source {
+    my ( $Self, $Code ) = @_;
+
+    return if $Self->is_disabled(Code => $Code);
+
+    my $DocumentationPresent = $Code =~ m{^\s*<File.+Location="doc/(?:de|en)}smx;
+
+    if (!$DocumentationPresent) {
+        die "Every OPM package needs to include documentation!";
+    }
+}
+
+1;
