60bce36f6df0d9064b7919811b42e16f7027313b
Martin Gruner <martin.gruner@otrs.com>
2013-08-27 15:57:36 +0200

    Ported check.

---
+++ TidyAll/Plugin/OTRS/Perl/DBObject.pm
@@ -0,0 +1,37 @@
+package TidyAll::Plugin::OTRS::Perl::DBObject;
+
+use strict;
+use warnings;
+
+use File::Basename;
+
+use base qw(TidyAll::Plugin::OTRS::Base);
+
+sub validate_source {
+    my ( $Self, $Code ) = @_;
+
+    return $Code if $Self->IsPluginDisabled(Code => $Code);
+
+    my ($ErrorMessage, $Counter);
+
+    LINE:
+    for my $Line ( split /\n/, $Code ) {
+        $Counter++;
+        next LINE if $Line !~ m{\{DBObject\}}smx;
+        next LINE if $Line =~ m{DBObject \s+    => \s \$Self->\{DBObject\} }smx;
+
+        $ErrorMessage .= "Line $Counter: $Line\n";
+    }
+
+    # Check for presence of shebang line
+    if ( $ErrorMessage ) {
+        die __PACKAGE__ . "\n" . <<EOF;
+Don't use the DBObject in frontend modules.
+$ErrorMessage
+EOF
+    }
+
+    return;
+}
+
+1;
