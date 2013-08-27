a5c5a394d9f070d524f421d3133696fa69ff2e24
Martin Gruner <martin.gruner@otrs.com>
2013-08-27 15:50:58 +0200

    Ported check.

---
+++ TidyAll/Plugin/OTRS/Perl/LayoutObject.pm
@@ -0,0 +1,24 @@
+package TidyAll::Plugin::OTRS::Perl::LayoutObject;
+
+use strict;
+use warnings;
+
+use File::Basename;
+
+use base qw(TidyAll::Plugin::OTRS::Base);
+
+sub validate_source {
+    my ( $Self, $Code ) = @_;
+
+    return $Code if $Self->IsPluginDisabled(Code => $Code);
+
+    # Check for presence of shebang line
+    if ( $Code =~ m{LayoutObject}xms ) {
+        die __PACKAGE__ . "\n" . <<EOF;
+Don't use the LayoutObject in bin/ or in Kernel/System.
+EOF
+    }
+    return;
+}
+
+1;
