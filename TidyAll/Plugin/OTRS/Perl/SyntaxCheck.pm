84b6f4964e422f34e3381f3f228ce78b8080f20d
Martin Gruner <martin.gruner@otrs.com>
2013-08-30 17:04:01 +0200

    Made test more robust.

--- TidyAll/Plugin/OTRS/Perl/SyntaxCheck.pm
+++ TidyAll/Plugin/OTRS/Perl/SyntaxCheck.pm
@@ -12,41 +12,56 @@ sub validate_source {
 
     return if $Self->IsPluginDisabled(Code => $Code);
 
-    my $TempFile = File::Temp->new();
-
-    my $CleanedSource;
+    my ($CleanedSource, $DeletableStatement);
 
     LINE:
     for my $Line ( split(/\n/, $Code) ) {
 
         $Line =~ s{\[gettimeofday\]}{1}smx;
 
-        # We'll skip all use *; statements exept a few because the modules cannot all be found
-        # at runtime.
+        # We'll skip all use *; statements exept a few because the modules cannot all be found at runtime.
         if (
-            $Line =~ m{ \A \s* use }xms
+            $Line =~ m{ \A \s* use \s+ }xms
             && $Line !~ m{\A \s* use \s+ (?: vars | constant | strict | warnings | Data (?! ::Validate ) | threads | Readonly | lib | FindBin | IO::Socket | File::Basename | Moo | Perl::Critic | Cwd ) }xms
         )
         {
+            $DeletableStatement = 1;
+        }
+
+        if ($DeletableStatement) {
             $Line = "#$Line";
         }
+
+        if ($Line =~ m{ ; \s* \z }xms) {
+            $DeletableStatement = 0;
+        }
+
         $CleanedSource .= $Line . "\n";
     }
 
+    #print STDERR $CleanedSource;
+
+    my $TempFile = File::Temp->new();
     print $TempFile $CleanedSource;
     $TempFile->flush();
 
     # syntax check
     my $ErrorMessage;
-    open my $In, '-|', "perl -cw " . $TempFile->filename() . " 2>&1" or die "FILTER: Can't open tempfile: $!\n";
-    while (my $Line = <$In>) {
+    my $FileHandle;
+    if (!open $FileHandle, '-|', "perl -cw " . $TempFile->filename() . " 2>&1") {
+        die __PACKAGE__ . "\nFILTER: Can't open tempfile: $!\n";
+    }
+
+    while (my $Line = <$FileHandle>) {
         if ($Line !~ /(syntax OK|used only once: possible typo)/) {
             $ErrorMessage .= $Line;
         }
     }
-    close $In;
+    close $FileHandle;
 
-    die $ErrorMessage if ($ErrorMessage);
+    if ($ErrorMessage) {
+        die __PACKAGE__ . "\n$ErrorMessage";
+    }
 }
 
 1;
