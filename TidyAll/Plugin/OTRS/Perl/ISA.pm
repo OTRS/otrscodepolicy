c3829172eb70d27f89452fcc46eb5a0d7f7104fd
Martin Gruner <martin.gruner@otrs.com>
2013-08-30 15:51:13 +0200

    Added new filter.

---
+++ TidyAll/Plugin/OTRS/Perl/ISA.pm
@@ -0,0 +1,38 @@
+package TidyAll::Plugin::OTRS::Perl::ISA;
+
+use strict;
+use warnings;
+
+use File::Basename;
+
+use base qw(TidyAll::Plugin::OTRS::Base);
+
+sub validate_source {
+    my ( $Self, $Code ) = @_;
+
+    return $Code if $Self->IsPluginDisabled(Code => $Code);
+
+    # Don't allow push @ISA.
+    if ( $Code =~ m{push\(?\s*\@ISA }xms ) {
+        die __PACKAGE__ . "\n" . <<EOF;
+Don't push to \@ISA, this can cause problems in persistent environments.
+Use Main::RequireBaseClass() instead.
+EOF
+    }
+    return;
+}
+
+sub transform_source {
+    my ( $Self, $Code ) = @_;
+
+    return $Code if $Self->IsPluginDisabled(Code => $Code);
+
+    # remove useless use vars qw(@ISA); (where ISA is not used)
+    if ( $Code !~ m{\@ISA.*\@ISA}smx ) {
+        $Code =~ s{^use \s+ vars \s+ qw\(\@ISA\);\n+}{}smx;
+    }
+
+    return $Code;
+}
+
+1;
