e620a210fa276a249bf7b10095938af75ec7147d
Martin Gruner <martin.gruner@otrs.com>
2013-09-03 17:05:58 +0200

    Ported check from OTRSRecode.

---
+++ TidyAll/Plugin/OTRS/Perl/SortKeys.pm
@@ -0,0 +1,33 @@
+package TidyAll::Plugin::OTRS::Perl::SortKeys;
+
+use strict;
+use warnings;
+
+use File::Basename;
+
+use base qw(TidyAll::Plugin::OTRS::Base);
+
+=head1
+
+This module inserts a sort statements to lines like
+
+    for my $Module (keys %Modules) ...
+
+because the keys randomness can be a source of problems
+that is hard to debug.
+
+=cut
+
+sub transform_source {
+    my ( $Self, $Code ) = @_;
+
+    return $Code if $Self->IsPluginDisabled(Code => $Code);
+    #return $Code if ($Self->IsFrameworkVersionLessThan(3, 3));
+
+    $Code =~ s{ ^ (\s* for \s+ my \s+ \$ \w+ \s+ \( \s*) keys \s+ }{$1sort keys }xmsg;
+    $Code =~ s{ ^ (\s* for \s+ \( \s*) keys \s+ }{$1sort keys }xmsg;
+
+    return $Code;
+}
+
+1;
