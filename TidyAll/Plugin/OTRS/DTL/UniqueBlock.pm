68f562e85fa1d69594fd276946fa77358839ae8e
Martin Gruner <martin.gruner@otrs.com>
2013-09-05 15:37:42 +0200

    Ported check.

---
+++ TidyAll/Plugin/OTRS/DTL/UniqueBlock.pm
@@ -0,0 +1,38 @@
+package TidyAll::Plugin::OTRS::DTL::UniqueBlock;
+
+use strict;
+use warnings;
+
+use base qw(TidyAll::Plugin::OTRS::Base);
+
+sub validate_source {
+    my ( $Self, $Code ) = @_;
+
+    return $Code if $Self->IsPluginDisabled(Code => $Code);
+    return if ($Self->IsFrameworkVersionLessThan(3, 2));
+
+    my ($Counter, $ErrorMessage, %BlockCounter);
+
+    for my $Line ( split /\n/, $Code ) {
+        if ($Line =~ m{ ^ \s*? <!-- \s dtl:block: (\w+) \s* -->}xms) {
+            $BlockCounter{$1}++;
+        }
+    }
+
+    for my $Block ( sort keys %BlockCounter ) {
+        if ( $BlockCounter{$Block} == 1 ) {
+            $ErrorMessage .= "Block usage error. You used the block '$Block' only one time, the closing block is missing.\n";
+        }
+        if ( $BlockCounter{$Block} > 2 ) {
+            $ErrorMessage .= "A block name should be unique. But you use the block '$Block'  for more than one time.\n";
+        }
+    }
+
+    if ( $ErrorMessage ) {
+        die __PACKAGE__ . "\n$ErrorMessage";
+    }
+
+    return;
+}
+
+1;
