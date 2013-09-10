ff1493e32f820b35a4c74d75ee9466ec5dc3dae7
Martin Gruner <martin.gruner@otrs.com>
2013-09-10 12:51:45 +0200

    Moved check to tidyall.

---
+++ TidyAll/Plugin/OTRS/Perl/Goto.pm
@@ -0,0 +1,47 @@
+# --
+# TidyAll/Plugin/OTRS/Perl/Goto.pm - code quality plugin
+# Copyright (C) 2001-2013 OTRS AG, http://otrs.com/
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file COPYING for license information (AGPL). If you
+# did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
+# --
+
+package TidyAll::Plugin::OTRS::Perl::Goto;
+## nofilter(TidyAll::Plugin::OTRS::Perl::Goto)
+
+use strict;
+use warnings;
+
+use File::Basename;
+
+use base qw(TidyAll::Plugin::OTRS::Base);
+
+sub validate_source {    ## no critic
+    my ( $Self, $Code ) = @_;
+
+    return if $Self->IsPluginDisabled( Code => $Code );
+    my ( $ErrorMessage, $Counter );
+
+    LINE:
+    for my $Line ( split /\n/, $Code ) {
+        $Counter++;
+
+        next LINE if $Line =~ m/^\s*\#/smx;
+
+        if ( $Line =~ m{ \s goto \s }smx ) {
+            $ErrorMessage .= "Line $Counter: $Line\n";
+        }
+    }
+
+    if ($ErrorMessage) {
+        die __PACKAGE__ . "\n" . <<EOF;
+Don't use 'goto' in Perl code.
+$ErrorMessage
+EOF
+    }
+
+    return;
+}
+
+1;
