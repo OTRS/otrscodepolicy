66d422e1b01d1a8c82b34cffa6de9f23b8bd019f
Martin Gruner <martin.gruner@otrs.com>
2013-09-10 13:01:41 +0200

    Moved check to tidyall

---
+++ TidyAll/Plugin/OTRS/Perl/Dumper.pm
@@ -0,0 +1,47 @@
+# --
+# TidyAll/Plugin/OTRS/Perl/Dumper.pm - code quality plugin
+# Copyright (C) 2001-2013 OTRS AG, http://otrs.com/
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file COPYING for license information (AGPL). If you
+# did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
+# --
+
+package TidyAll::Plugin::OTRS::Perl::Dumper;
+## nofilter(TidyAll::Plugin::OTRS::Perl::Dumper)
+use strict;
+use warnings;
+
+use File::Basename;
+
+use base qw(TidyAll::Plugin::OTRS::Base);
+
+sub validate_source {    ## no critic
+    my ( $Self, $Code ) = @_;
+
+    return if $Self->IsPluginDisabled( Code => $Code );
+    my ( $ErrorMessage, $Counter );
+
+    LINE:
+    for my $Line ( split /\n/, $Code ) {
+        $Counter++;
+
+        next LINE if $Line =~ m/^\s*\#/smx;
+
+        if ( $Line =~ m{Data::Dumper|(->|\s)Dumper\(}smx ) {
+            $ErrorMessage .= "Line $Counter: $Line\n";
+        }
+    }
+
+    if ($ErrorMessage) {
+        die __PACKAGE__ . "\n" . <<EOF;
+Use Main::Dump() instead of Dumper().
+Don't forget to remove debug code like print STDERR Dumper(...).
+$ErrorMessage
+EOF
+    }
+
+    return;
+}
+
+1;
