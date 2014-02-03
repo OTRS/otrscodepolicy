619dd8511af86a14f18af5c9114591c32d5a69de
Martin Gruner <martin.gruner@otrs.com>
2014-02-03 13:27:02 +0100

    Added new check for use utf8; in Language files for OTRS 3.4+.

---
+++ Kernel/TidyAll/Plugin/OTRS/Perl/TranslationEncoding.pm
@@ -0,0 +1,34 @@
+# --
+# TidyAll/Plugin/OTRS/Perl/TranslationEncoding.pm - code quality plugin
+# Copyright (C) 2001-2014 OTRS AG, http://otrs.com/
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file COPYING for license information (AGPL). If you
+# did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
+# --
+
+package TidyAll::Plugin::OTRS::Perl::TranslationEncoding;
+
+use strict;
+use warnings;
+
+use File::Basename;
+
+use base qw(TidyAll::Plugin::OTRS::Perl);
+
+sub validate_source {    ## no critic
+    my ( $Self, $Code ) = @_;
+
+    return $Code if $Self->IsPluginDisabled( Code => $Code );
+    return $Code if $Self->IsFrameworkVersionLessThan( 3, 4 );
+
+    if ( $Code !~ m{^[ \t]*use\s+utf8;}mx ) {
+        die __PACKAGE__ . "\n" . <<EOF;
+All language files must be encoded in "utf-8", and include the "use utf8;" Perl pragma.
+EOF
+    }
+
+    return;
+}
+
+1;
