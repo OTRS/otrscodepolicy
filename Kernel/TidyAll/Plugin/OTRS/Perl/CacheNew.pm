53cc297496d57f12abac8ed92ff37836b26ef1ea
Martin Gruner <martin.gruner@otrs.com>
2014-08-05 13:49:12 +0200

    Disallow local instances of the Cache object, force fetching from OM.

---
+++ Kernel/TidyAll/Plugin/OTRS/Perl/CacheNew.pm
@@ -0,0 +1,49 @@
+# --
+# TidyAll/Plugin/OTRS/Perl/CacheNew.pm - code quality plugin
+# Copyright (C) 2001-2014 OTRS AG, http://otrs.com/
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file COPYING for license information (AGPL). If you
+# did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
+# --
+
+package TidyAll::Plugin::OTRS::Perl::CacheNew;
+
+use strict;
+use warnings;
+
+use base qw(TidyAll::Plugin::OTRS::Perl);
+
+## nofilter(TidyAll::Plugin::OTRS::Perl::CacheNew)
+## nofilter(TidyAll::Plugin::OTRS::Perl::ObjectDependencies)
+
+sub validate_source {    ## no critic
+    my ( $Self, $Code ) = @_;
+
+    return if $Self->IsPluginDisabled( Code => $Code );
+    return if $Self->IsFrameworkVersionLessThan( 3, 4 );
+
+    $Code = $Self->StripPod( Code => $Code );
+
+    my ( $ErrorMessage, $Counter );
+
+    LINE:
+    for my $Line ( split /\n/, $Code ) {
+        $Counter++;
+
+        if ( $Line =~ m/Kernel::System::Cache->new/smx ) {
+            $ErrorMessage .= "Line $Counter: $Line\n";
+        }
+    }
+
+    if ($ErrorMessage) {
+        die __PACKAGE__ . "\n" . <<EOF;
+Don't create your own instance of Kernel::System::Cache.pm, but use $Kernel::OM->Get('Kernel::System::Cache') instead.
+$ErrorMessage
+EOF
+    }
+
+    return;
+}
+
+1;
---
+++ scripts/test/OTRSCodePolicyPlugins/Perl/CacheNew.t
@@ -0,0 +1,43 @@
+# --
+# OTRSCodePolicyPlugins/Perl/CacheNew.t - code policy self tests
+# Copyright (C) 2001-2014 OTRS AG, http://otrs.com/
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file COPYING for license information (AGPL). If you
+# did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
+# --
+use strict;
+use warnings;
+## nofilter(TidyAll::Plugin::OTRS::Common::CustomizationMarkers);
+
+use vars (qw($Self));
+use utf8;
+
+use scripts::test::OTRSCodePolicyPlugins;
+
+my @Tests = (
+    {
+        Name      => 'CacheNew, forbidden',
+        Filename  => 'test.pl',
+        Plugins   => [qw(TidyAll::Plugin::OTRS::Perl::CacheNew)],
+        Framework => '3.4',
+        Source    => <<'EOF',
+Kernel::System::Cache->new(%{$Self});
+EOF
+        Exception => 1,
+    },
+    {
+        Name      => 'CacheNew, ok',
+        Filename  => 'test.pl',
+        Plugins   => [qw(TidyAll::Plugin::OTRS::Perl::CacheNew)],
+        Framework => '3.4',
+        Source    => <<'EOF',
+$Kernel::OM->Get('Kernel::System::Cache');
+EOF
+        Exception => 0,
+    },
+);
+
+$Self->scripts::test::OTRSCodePolicyPlugins::Run( Tests => \@Tests );
+
+1;
