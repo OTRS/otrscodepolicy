1a707bb89bad85712a64a4aac52bbde434b74cba
Martin Gruner <martin.gruner@otrs.com>
2014-08-29 13:46:16 +0200

    Added PO file validator.

---
+++ Kernel/TidyAll/Plugin/OTRS/PO/msgfmt.pm
@@ -0,0 +1,45 @@
+# --
+# TidyAll/Plugin/OTRS/PO/msgfmt.pm - code quality plugin
+# Copyright (C) 2001-2014 OTRS AG, http://otrs.com/
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file COPYING for license information (AGPL). If you
+# did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
+# --
+
+package TidyAll::Plugin::OTRS::PO::msgfmt;
+
+#
+# Use msgfmt to check the basic validity of .po and .pot files.
+#
+
+use strict;
+use warnings;
+
+use Capture::Tiny qw(capture_merged);
+use base qw(TidyAll::Plugin::OTRS::Base);
+
+sub _build_cmd {    ## no critic
+    return 'msgfmt -c -o /dev/null'
+}
+
+sub validate_file {    ## no critic
+    my ( $Self, $Filename ) = @_;
+
+    return if $Self->IsPluginDisabled( Filename => $Filename );
+    return if $Self->IsFrameworkVersionLessThan( 4, 0 );
+
+    my $Command = sprintf( "%s %s %s", $Self->cmd(), $Self->argv(), $Filename );
+    my ( $Output, @Result ) = capture_merged { system($Command) };
+
+    # if execution failed, warn about installing package
+    if ( $Result[0] == -1 ) {
+        print STDERR "'msgfmt' is not installed. Please install 'gettext'.\n";
+    }
+
+    if ( @Result && $Result[0] ) {
+        die __PACKAGE__ . "\n$Output\n";    # non-zero exit code
+    }
+}
+
+1;
