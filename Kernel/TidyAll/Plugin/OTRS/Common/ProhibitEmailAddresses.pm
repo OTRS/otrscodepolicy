c3c2f0ad00487e44b16a523dc0b08abb9cfcecd9
Martin Gruner <martin.gruner@otrs.com>
2014-10-21 10:57:16 +0200

    Add new plugin to prohibit certain email addresses.

---
+++ Kernel/TidyAll/Plugin/OTRS/Common/ProhibitEmailAddresses.pm
@@ -0,0 +1,49 @@
+# --
+# TidyAll/Plugin/OTRS/Common/ProhibitEmailAddresses.pm - code quality plugin
+# Copyright (C) 2001-2014 OTRS AG, http://otrs.com/
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file COPYING for license information (AGPL). If you
+# did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
+# --
+
+package TidyAll::Plugin::OTRS::Common::ProhibitEmailAddresses;
+
+use strict;
+use warnings;
+
+use File::Basename;
+
+use base qw(TidyAll::Plugin::OTRS::Base);
+
+=head1 SYNOPSIS
+
+This plugin disallows problematic email addresses.
+
+=cut
+
+sub validate_source {    ## no critic
+    my ( $Self, $Code ) = @_;
+
+    return $Code if $Self->IsPluginDisabled( Code => $Code );
+    return $Code if $Self->IsFrameworkVersionLessThan( 3, 3 );
+
+    my ( $Counter, $ErrorMessage );
+
+    for my $Line ( split /\n/, $Code ) {
+        $Counter++;
+        if ( $Line =~ /support\@otrs\.(?:com|de)/ismx ) {
+            $ErrorMessage .= "Line $Counter: $Line\n";
+        }
+    }
+
+    if ($ErrorMessage) {
+        die __PACKAGE__ . "\n" . <<EOF;
+Don't use support\@otrs.com in any source files or documents as this address has SPAM problems.
+$ErrorMessage
+EOF
+    }
+    return;
+}
+
+1;
