dda6806cd28125e56cdaebed981885218355d48d
Martin Gruner <martin.gruner@otrs.com>
2015-03-27 08:41:56 +0100

    Tidied.

--- Kernel/TidyAll/Plugin/OTRS/JavaScript/ESLint.pm
+++ Kernel/TidyAll/Plugin/OTRS/JavaScript/ESLint.pm
@@ -25,20 +25,21 @@ sub validate_file {    ## no critic
     return if $Self->IsPluginDisabled( Filename => $Filename );
     return if $Self->IsFrameworkVersionLessThan( 5, 0 );
 
-    if (!$ESLintPath) {
+    if ( !$ESLintPath ) {
+
         # On some systems (Ubuntu) nodejs is called /usr/bin/nodejs instead of /usr/bin/node,
         #   which can lead to problems with calling the node scripts directly. Therefore we
         #   determine the nodejs binary and call it directly.
         $NodePath = `which nodejs` || `which node`;
         chomp $NodePath;
-        if (!$NodePath) {
+        if ( !$NodePath ) {
             print STDERR "Could not find 'nodejs' binary, skipping ESLint tests.\n";
             return;
         }
 
         $ESLintPath = `which eslint`;
         chomp $ESLintPath;
-        if (!$ESLintPath) {
+        if ( !$ESLintPath ) {
             print STDERR "Could not find 'eslint' script, skipping ESLint tests.\n";
             print STDERR "Install nodejs and run 'npm -g i eslint' to install eslint.\n";
             return;
@@ -50,9 +51,9 @@ sub validate_file {    ## no critic
         # force minimum version 0.17.1
         my $ESLintVersion = `$NodePath $ESLintPath -v`;
         chomp $ESLintVersion;
-        my ($Major, $Minor, $Patch) = $ESLintVersion =~ m{v(\d+)[.](\d+)[.](\d+)};
-        my $Compare = sprintf("%03d%03d%03d", $Major, $Minor, $Patch);
-        if (!length($Major) || $Compare < 17_001) {
+        my ( $Major, $Minor, $Patch ) = $ESLintVersion =~ m{v(\d+)[.](\d+)[.](\d+)};
+        my $Compare = sprintf( "%03d%03d%03d", $Major, $Minor, $Patch );
+        if ( !length($Major) || $Compare < 17_001 ) {
             undef $ESLintPath;
             die "Your eslint version ($ESLintVersion) is outdated. Please update with 'npm -g update eslint'.\n";
         }
