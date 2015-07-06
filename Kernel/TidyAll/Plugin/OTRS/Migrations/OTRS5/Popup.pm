c2e833ea6602249f4285777413d93eac146798f0
Marc Nilius <marc.nilius@libertello.de>
2015-07-06 14:40:19 +0200

    Added TidyAll plugin to check correct wording in OTRS Popups for OTRS 5.

---
+++ Kernel/TidyAll/Plugin/OTRS/Migrations/OTRS5/Popup.pm
@@ -0,0 +1,45 @@
+# --
+# Copyright (C) 2001-2015 OTRS AG, http://otrs.com/
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file COPYING for license information (AGPL). If you
+# did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
+# --
+
+package TidyAll::Plugin::OTRS::Migrations::OTRS5::Popup;
+
+use strict;
+use warnings;
+
+use base qw(TidyAll::Plugin::OTRS::Base);
+
+sub validate_source {    ## no critic
+    my ( $Self, $Code ) = @_;
+
+    return $Code if $Self->IsPluginDisabled( Code => $Code );
+    return if $Self->IsFrameworkVersionLessThan( 5, 0 );
+
+    my ( $Counter, $ErrorMessage );
+
+    for my $Line ( split /\n/, $Code ) {
+        $Counter++;
+
+        # look for forbidden text in popup header
+        # text should be "cancel & close" or "undo & close"
+        # but not "xyz & close window" anymore
+        if ( $Line =~ m{\[% Translate\("(Undo & close window|Cancel & close window|Close window)}smi ) {
+            $ErrorMessage .= "Line $Counter: $Line\n";
+        }
+    }
+
+    if ($ErrorMessage) {
+        die __PACKAGE__ . "\n" . <<EOF;
+Popup close notice should not contain the word "window".
+$ErrorMessage
+EOF
+    }
+
+    return;
+}
+
+1;
