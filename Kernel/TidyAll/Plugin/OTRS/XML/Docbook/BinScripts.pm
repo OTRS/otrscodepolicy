dac654054543de2452570ac7eee4b0242d7e731b
Carlos Rodriguez <carlos.rodriguez@otrs.com>
2015-08-14 10:26:48 -0500

    Added check for bin scripts in documentation.

---
+++ Kernel/TidyAll/Plugin/OTRS/XML/Docbook/BinScripts.pm
@@ -0,0 +1,61 @@
+# --
+# Copyright (C) 2001-2015 OTRS AG, http://otrs.com/
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file COPYING for license information (AGPL). If you
+# did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
+# --
+
+package TidyAll::Plugin::OTRS::XML::Docbook::BinScripts;
+
+use strict;
+use warnings;
+
+use File::Basename;
+
+use base qw(TidyAll::Plugin::OTRS::Base);
+
+=head1 SYNOPSIS
+
+This plugin disallows problematic email addresses.
+
+=cut
+
+sub validate_source {    ## no critic
+    my ( $Self, $Code ) = @_;
+
+    return $Code if $Self->IsPluginDisabled( Code => $Code );
+    return $Code if $Self->IsFrameworkVersionLessThan( 5, 0 );
+
+    my %AllowedFiles = (
+        'otrs.CheckModules.pl'   => 1,
+        'otrs.CheckSum.pl'       => 1,
+        'otrs.CodePolicy.pl'     => 1,
+        'otrs.Console.pl'        => 1,
+        'otrs.Daemon.pl'         => 1,
+        'otrs.SetPermissions.pl' => 1,
+    );
+
+    my ( $Counter, $ErrorMessage );
+
+    LINE:
+    for my $Line ( split /\n/, $Code ) {
+        $Counter++;
+        if ( $Line =~ /bin\/(otrs\.\w+\.pl)/ismx ) {
+
+            next LINE if $AllowedFiles{$1};
+
+            $ErrorMessage .= "Line $Counter: $Line\n";
+        }
+    }
+
+    if ($ErrorMessage) {
+        die __PACKAGE__ . "\n" . <<EOF;
+Don't use old bin scrips in documentation.
+$ErrorMessage
+EOF
+    }
+    return;
+}
+
+1;
