65272d87f3356b6fcc7cb98a4b781797b52fdb1b
Marc Nilius <marc.nilius@libertello.de>
2015-10-01 12:09:19 +0200

    Added new rule to check and prevent usage of JavaScript unload event.

---
+++ Kernel/TidyAll/Plugin/OTRS/JavaScript/UnloadEvent.pm
@@ -0,0 +1,38 @@
+# --
+# Copyright (C) 2001-2015 OTRS AG, http://otrs.com/
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file COPYING for license information (AGPL). If you
+# did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
+# --
+
+package TidyAll::Plugin::OTRS::JavaScript::UnloadEvent;
+
+use strict;
+use warnings;
+
+use base qw(TidyAll::Plugin::OTRS::Base);
+
+sub validate_source {    ## no critic
+    my ( $Self, $Code ) = @_;
+
+    return if $Self->IsPluginDisabled( Code => $Code );
+    return if $Self->IsFrameworkVersionLessThan( 5, 0 );
+
+    my $ErrorMessage;
+    my $Counter;
+
+    for my $Line ( split( /\n/, $Code ) ) {
+        $Counter++;
+        if ( $Line =~ m{ \.bind\('unload }xms || $Line =~ m{ \.on\('unload }xms ) {
+            $ErrorMessage
+                .= "ERROR: Found window unload event in line( $Counter ): $Line\n";
+            $ErrorMessage .= "Please use Core.App.BindWindowUnloadEvent() for cross-browser compatibility.\n";
+        }
+    }
+    if ($ErrorMessage) {
+        die __PACKAGE__ . "\n$ErrorMessage";
+    }
+}
+
+1;
