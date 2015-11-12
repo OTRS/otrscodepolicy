b95a27e2ffb842d5026a755a6d798230276e4b0e
Marc Nilius <marc.nilius@libertello.de>
2015-11-12 12:33:12 +0100

    Added file name check for javascript files (including unit test files).

---
+++ Kernel/TidyAll/Plugin/OTRS/JavaScript/FileName.pm
@@ -0,0 +1,45 @@
+# --
+# Copyright (C) 2001-2015 OTRS AG, http://otrs.com/
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file COPYING for license information (AGPL). If you
+# did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
+# --
+
+package TidyAll::Plugin::OTRS::JavaScript::FileName;
+
+use strict;
+use warnings;
+
+use File::Basename;
+
+use base qw(TidyAll::Plugin::OTRS::Base);
+
+sub validate_file {    ## no critic
+    my ( $Self, $Filename ) = @_;
+
+    return if $Self->IsPluginDisabled( Filename => $Filename );
+    return if $Self->IsFrameworkVersionLessThan( 5, 0 );
+
+    my $Code = $Self->_GetFileContents($Filename);
+    my $NameOfFile = substr( basename($Filename), 0, -3 ); # cut off .js
+
+    my $ErrorMessage;
+    my $Counter;
+
+    for my $Line ( split( /\n/, $Code ) ) {
+        $Counter++;
+
+        if ( $Line =~ m{^([^= ]+)\s*=\s*\(function\s*\(TargetNS\)\s*\{ }xms ) {
+
+            if ( $1 ne $NameOfFile ) {
+                die __PACKAGE__ . "\n" . <<EOF;
+The file name ($NameOfFile.js) is not equal to the name of the JavaScript namespace ($1).
+EOF
+            }
+        }
+    }
+
+}
+
+1;
---
+++ Kernel/TidyAll/Plugin/OTRS/JavaScript/FileNameUnitTest.pm
@@ -0,0 +1,45 @@
+# --
+# Copyright (C) 2001-2015 OTRS AG, http://otrs.com/
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file COPYING for license information (AGPL). If you
+# did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
+# --
+
+package TidyAll::Plugin::OTRS::JavaScript::FileNameUnitTest;
+
+use strict;
+use warnings;
+
+use File::Basename;
+
+use base qw(TidyAll::Plugin::OTRS::Base);
+
+sub validate_file {    ## no critic
+    my ( $Self, $Filename ) = @_;
+
+    return if $Self->IsPluginDisabled( Filename => $Filename );
+    return if $Self->IsFrameworkVersionLessThan( 5, 0 );
+
+    my $Code = $Self->_GetFileContents($Filename);
+    my $NameOfFile = substr( basename($Filename), 0, -3 ); # cut off .js
+
+    my $ErrorMessage;
+    my $Counter;
+
+    for my $Line ( split( /\n/, $Code ) ) {
+        $Counter++;
+
+        if ( $Line =~ m{^([^= ]+)\s*=\s*\(function\s*\(Namespace\)\s*\{ }xms ) {
+
+            if ( $1 . ".UnitTest" ne $NameOfFile ) {
+                die __PACKAGE__ . "\n" . <<EOF;
+The file name ($NameOfFile.js) is not correct for the unit tests of the JavaScript namespace ($1). Must be $1.UnitTest.js.
+EOF
+            }
+        }
+    }
+
+}
+
+1;
