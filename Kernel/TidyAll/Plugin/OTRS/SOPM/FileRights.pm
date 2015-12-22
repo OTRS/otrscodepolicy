ccec19baf5abdc6fb8d062ba97ed04dd31ed852a
Martin Gruner <martin.gruner@otrs.com>
2015-12-22 14:56:20 +0100

    Improved file permission check to be more strict for OTRS 6+.

--- Kernel/TidyAll/Plugin/OTRS/SOPM/FileRights.pm
+++ Kernel/TidyAll/Plugin/OTRS/SOPM/FileRights.pm
@@ -18,6 +18,16 @@ sub validate_source {    ## no critic
 
     return if $Self->IsPluginDisabled( Code => $Code );
 
+    my $ExecutablePermissionCheck = qr{Permission="[75]{2}0"};
+    my $StaticPermissionCheck     = qr{Permission="[64]{2}0"};
+
+    # A little more lenient before OTRS 6 (world permissions)
+    if ( $Self->IsFrameworkVersionLessThan( 6, 0 ) ) {
+        $ExecutablePermissionCheck = qr{Permission="[750]{3}"};
+        $StaticPermissionCheck     = qr{Permission="[640]{3}"};
+    }
+
+
     my ( $ErrorMessage, $Counter );
 
     LINE:
@@ -26,13 +36,13 @@ sub validate_source {    ## no critic
         next LINE if $Line !~ m/<File.*\/>/;
         if ( $Line =~ m/<File.*Location="([^"]+)".*\/>/ ) {
             if ( $1 && $1 =~ /\.(pl|sh|fpl|psgi|sh)$/ ) {
-                if ( $Line !~ /Permission="[750]{3}"/ ) {
+                if ( $Line !~ $ExecutablePermissionCheck ) {
                     $ErrorMessage .= "Line $Counter: $Line\n";
                 }
             }
 
             else {
-                if ( $Line !~ /Permission="[640]{3}"/ ) {
+                if ( $Line !~ $StaticPermissionCheck ) {
                     $ErrorMessage .= "Line $Counter: $Line\n";
                 }
             }
@@ -41,7 +51,7 @@ sub validate_source {    ## no critic
 
     if ($ErrorMessage) {
         die __PACKAGE__ . "\n" . <<EOF;
-A <File>-Tag has wrong permissions. Script files normally need 755 rights, the others 644.
+A <File>-Tag has wrong permissions. Script files normally need 750 rights, the others 640.
 $ErrorMessage
 EOF
     }
