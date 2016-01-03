a020e3acef7c37eb2b2041222523e869a9fefca1
Stefan Bedorf <stefan.bedorf@otrs.com>
2016-01-03 20:41:03 +0100

    Updated copyright date.

--- Kernel/TidyAll/Plugin/OTRS/SOPM/FileRights.pm
+++ Kernel/TidyAll/Plugin/OTRS/SOPM/FileRights.pm
@@ -1,5 +1,5 @@
 # --
-# Copyright (C) 2001-2015 OTRS AG, http://otrs.com/
+# Copyright (C) 2001-2016 OTRS AG, http://otrs.com/
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -26,7 +26,6 @@ sub validate_source {    ## no critic
         $ExecutablePermissionCheck = qr{Permission="[750]{3}"};
         $StaticPermissionCheck     = qr{Permission="[640]{3}"};
     }
-
 
     my ( $ErrorMessage, $Counter );
 
