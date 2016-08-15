265dafa0b51b8ae4d5a096b2989c482c430c8655
Martin Gruner <martin.gruner@otrs.com>
2016-08-15 11:00:13 +0200

    Added new code policy plugin to disallow config changes in selenium tests via SysConfigObject.

---
+++ Kernel/TidyAll/Plugin/OTRS/Perl/UnitTestConfigChanges.pm
@@ -0,0 +1,45 @@
+# --
+# Copyright (C) 2001-2016 OTRS AG, http://otrs.com/
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file COPYING for license information (AGPL). If you
+# did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
+# --
+
+package TidyAll::Plugin::OTRS::Perl::UnitTestConfigChanges;
+
+use strict;
+use warnings;
+
+use File::Basename;
+
+use base qw(TidyAll::Plugin::OTRS::Perl);
+
+our $ObjectManagerDisabled = 1;
+
+# Make sure Selenium tests only modify the configuration via $Helper->ConfigSettingUpdate().
+
+sub validate_source {    ## no critic
+    my ( $Self, $Code ) = @_;
+
+    return if $Self->IsPluginDisabled( Code => $Code );
+    return if $Self->IsFrameworkVersionLessThan( 6, 0 );
+
+    my ( $ErrorMessage, $Counter );
+
+    if ( $Code =~ m{->ConfigItemUpdate|->ConfigItemReset}smx ) {
+        $ErrorMessage .= "Line $Counter: $Code\n";
+    }
+    if ($ErrorMessage) {
+        die __PACKAGE__ . "\n" . <<'EOF';
+Selenium tests should modify the system configuration exclusively via
+$Helper->ConfigSettingUpdate() (it has the same API as ConfigSettingUpdate()).
+This also makes "sleep" statements for mod_perl unneeded.
+$ErrorMessage
+EOF
+    }
+
+    return;
+}
+
+1;
