e73f49847c3b91490076d8e55dfe792213bb393a
Stefan Bedorf <stefan.bedorf@otrs.com>
2016-09-22 10:39:04 +0200

    Remove contact chapter from documentations.

---
+++ Kernel/TidyAll/Plugin/OTRS/XML/Docbook/RemoveContactChapter.pm
@@ -0,0 +1,34 @@
+# --
+# Copyright (C) 2001-2016 OTRS AG, http://otrs.com/
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file COPYING for license information (AGPL). If you
+# did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
+# --
+
+package TidyAll::Plugin::OTRS::XML::Docbook::RemoveContactChapter;
+
+use strict;
+use warnings;
+
+use base qw(TidyAll::Plugin::OTRS::Base);
+
+sub transform_source {    ## no critic
+    my ( $Self, $Code ) = @_;
+
+    return $Code if $Self->IsPluginDisabled( Code => $Code );
+
+    # Check for 'Contact' / Kontakt' chapter.
+    return $Code if $Code !~ m{ <chapter> [ \n]* <title> [ ]* (?: Contact | Kontakt ) [ ]* </title }xms;
+
+    # remove chapter
+    $Code =~ s{
+        <chapter> [ \n]*
+        <title> [ ]* (?: Contact | Kontakt ) [ ]* </title>
+        ( (?! </chapter> ). )* </chapter> [ \n]*
+    }{}xms;
+
+    return $Code;
+}
+
+1;
