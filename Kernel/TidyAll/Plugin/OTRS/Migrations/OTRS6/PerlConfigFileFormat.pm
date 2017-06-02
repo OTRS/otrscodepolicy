cfbfe9b029ea70db9e55cd11ce72c8c1b4edae86
Martin Gruner <martin.gruner@otrs.com>
2017-06-02 12:11:16 +0200

    Added new check to enforce new perl configuration file format for OTRS 6+.

---
+++ Kernel/TidyAll/Plugin/OTRS/Migrations/OTRS6/PerlConfigFileFormat.pm
@@ -0,0 +1,32 @@
+# --
+# Copyright (C) 2001-2017 OTRS AG, http://otrs.com/
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file COPYING for license information (AGPL). If you
+# did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
+# --
+
+package TidyAll::Plugin::OTRS::Migrations::OTRS6::PerlConfigFileFormat;
+
+use strict;
+use warnings;
+
+use parent qw(TidyAll::Plugin::OTRS::Base);
+
+sub validate_source {    ## no critic
+    my ( $Self, $Code ) = @_;
+
+    return $Code if $Self->IsPluginDisabled( Code => $Code );
+    return if $Self->IsFrameworkVersionLessThan( 6, 0 );
+
+    if ( $Code !~ m{^package \s}smx || $Code !~ m{^sub \s+ Load}smx ) {
+
+        die __PACKAGE__ . "\n" . <<EOF;
+Perl configuration files for OTRS 6+ must use the the new format like Kernel/Config/Files/ZZZAAuto.pm (they need to be created as a package with a Load() method).
+EOF
+    }
+
+    return;
+}
+
+1;
