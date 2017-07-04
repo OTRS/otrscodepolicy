c5fc4f1f15eb851a21cf2c94a5f5359829173f02
Martin Gruner <martin.gruner@otrs.com>
2017-07-04 15:55:14 +0200

    Refactored navigation validation module.

--- Kernel/TidyAll/Plugin/OTRS/XML/Configuration/Navigation.pm
+++ Kernel/TidyAll/Plugin/OTRS/XML/Configuration/Navigation.pm
@@ -34,64 +34,51 @@ sub validate_source {    ## no critic
 
         next LINE if !$NavigationContent;
 
-        my @NavigationArray = split qr{::}, $NavigationContent;
+        my @Rules = (
+            {
+                Name                   => 'Valid toplevel entries',
+                MatchSettingName       => qr{.*},
+                MatchNavigationValue   => qr{.*},
+                RequireNavigationMatch => qr{^(CloudService|Core|Daemon|GenericInterface|Frontend)(::|$)},
+                ErrorMessage =>
+                    'Invalid top level group found (only CloudService|Core|Daemon|GenericInterface|Frontend are allowed).',
+            },
+            {
+                Name                 => 'Valid Frontend subgroups',
+                MatchSettingName     => qr{.*},
+                MatchNavigationValue => qr{^Frontend},                # no entries allowed in "Frontend" directly
+                RequireNavigationMatch => qr{^Frontend::(Admin|Agent|Base|Customer|Public)(::|$)},
+                ErrorMessage =>
+                    'Invalid top Frontend subgroup found (only Admin|Agent|Base|Customer|Public are allowed).',
+            },
+            {
+                Name                   => 'Event handlers',
+                MatchSettingName       => qr{::EventModule},
+                MatchNavigationValue   => qr{.*},
+                RequireNavigationMatch => qr{^Core::Event::},
+                ErrorMessage           => "Event handler registrations should be grouped in 'Core::Event::*.",
+            },
+            {
+                Name                   => 'Main Loader config',
+                MatchSettingName       => qr{^Loader::(Agent|Customer|Enabled)},
+                MatchNavigationValue   => qr{.*},
+                RequireNavigationMatch => qr{^Frontend::Base::Loader},
+                ErrorMessage           => "Main Loader settings should be grouped in 'Frontend::Base::Loader'.",
+            },
 
-        my %ValidToplevelEntries = (
-            CloudService     => 1,
-            Core             => 1,
-            Daemon           => 1,
-            GenericInterface => 1,
-            Frontend         => 1,
         );
 
-        if ( !$ValidToplevelEntries{ $NavigationArray[0] } ) {
-            $ErrorMessage .= sprintf "'%s' is not one of the allowed top level navigation entries (%s)\n",
-                $NavigationArray[0], join( ', ', sort keys %ValidToplevelEntries );
-            $ErrorMessage .= "Line $Counter: $Line\n";
-        }
+        RULE:
+        for my $Rule (@Rules) {
+            next RULE if $CurrentSettingName !~ $Rule->{MatchSettingName};
+            next RULE if $NavigationContent !~ $Rule->{MatchNavigationValue};
 
-        if ( $NavigationArray[0] eq 'Frontend' && $NavigationArray[1] ) {
-
-            my %ValidFrontendEntries = (
-                Admin    => 1,
-                Agent    => 1,
-                Base     => 1,
-                Customer => 1,
-                Public   => 1,
-            );
-
-            if ( !$ValidFrontendEntries{ $NavigationArray[1] } ) {
-                $ErrorMessage .= sprintf "'%s' is not one of the allowed Frontend navigation entries (%s)\n",
-                    $NavigationArray[1], join( ', ', sort keys %ValidFrontendEntries );
+            if ( $NavigationContent !~ $Rule->{RequireNavigationMatch} ) {
+                $ErrorMessage
+                    .= "Invalid navigation value found for setting $CurrentSettingName: $Rule->{ErrorMessage}\n";
                 $ErrorMessage .= "Line $Counter: $Line\n";
             }
         }
-
-        if ( $CurrentSettingName =~ m{::EventModule} ) {
-            if (
-                @NavigationArray != 3
-                || $NavigationArray[0] ne 'Core'
-                || $NavigationArray[1] ne 'Event'
-                )
-            {
-                $ErrorMessage .= "Event handler registrations should be grouped in 'Core::Event::*'\n";
-                $ErrorMessage .= "Line $Counter: $Line\n";
-
-            }
-        }
-
-        # if ($CurrentSettingName =~ m{^Loader}) {
-        #     if (
-        #         @NavigationArray != 3
-        #         || $NavigationArray[0] ne 'Frontend'
-        #         || $NavigationArray[1] ne 'Base'
-        #         || $NavigationArray[1] ne 'Loader'
-        #     ) {
-        #         $ErrorMessage .= "Loader settings should be grouped in 'Frontend::Base::Loader'\n";
-        #         $ErrorMessage .= "Line $Counter: $Line\n";
-        #     }
-        # }
-
     }
 
     if ($ErrorMessage) {
