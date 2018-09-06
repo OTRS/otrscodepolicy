de5fa1dc0bc25ad7b97473b4eeee9448fa21bfcb
Manuel Hecht <manuel.hecht@otrs.com>
2018-09-06 22:33:33 +0200

    Added CRLFLineEndings.pm.

---
+++ Kernel/TidyAll/Plugin/OTRS/Common/CRLFLineEndings.pm
@@ -0,0 +1,47 @@
+# --
+# Copyright (C) 2001-2018 OTRS AG, https://otrs.com/
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file COPYING for license information (GPL). If you
+# did not receive this file, see https://www.gnu.org/licenses/gpl-3.0.txt.
+# --
+
+package TidyAll::Plugin::OTRS::Common::CRLFLineEndings;
+
+use strict;
+use warnings;
+
+use File::Basename;
+
+use parent qw(TidyAll::Plugin::OTRS::Base);
+
+=head1 SYNOPSIS
+
+This plugin converts files with CRLF line endings.
+
+=cut
+
+sub transform_source {    ## no critic
+    my ( $Self, $Code ) = @_;
+
+    return $Code if $Self->IsPluginDisabled( Code => $Code );
+    return $Code if $Self->IsFrameworkVersionLessThan( 5, 0 );
+
+    # Remove CRLF line endings.
+    $Code =~ s{ \r $ }{}xmsg;
+
+    return $Code;
+}
+
+sub validate_source {    ## no critic
+    my ( $Self, $Code ) = @_;
+
+    return $Code if $Self->IsPluginDisabled( Code => $Code );
+    return $Code if $Self->IsFrameworkVersionLessThan( 5, 0 );
+
+    die __PACKAGE__ . "\nFound CRLF line endings!" if $Code =~ m{ \r $ }xmsg;
+
+    return $Code;
+}
+
+1;
