35099dcd9521eb174c4b47f737a033a570c49757
Martin Gruner <martin.gruner@otrs.com>
2018-09-14 11:00:06 +0200

    Added (curently inactive) plugin to prevent the usage of body data in HEAD/GET endpoints.

---
+++ Kernel/TidyAll/Plugin/OTRS/Perl/WebApp/HeadGetWithoutBody.pm
@@ -0,0 +1,38 @@
+# --
+# Copyright (C) 2001-2018 OTRS AG, https://otrs.com/
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file COPYING for license information (GPL). If you
+# did not receive this file, see https://www.gnu.org/licenses/gpl-3.0.txt.
+# --
+
+package TidyAll::Plugin::OTRS::Perl::WebApp::HeadGetWithoutBody;
+
+use strict;
+use warnings;
+
+use parent qw(TidyAll::Plugin::OTRS::Perl);
+
+sub validate_source {    ## no critic
+    my ( $Self, $Code ) = @_;
+
+    #
+    # TODO: activate when changes have landed in the framework.
+    #
+    ## no critic
+    return;
+
+    return $Code if $Self->IsPluginDisabled( Code => $Code );
+    return $Code if $Self->IsFrameworkVersionLessThan( 7, 0 );
+
+    if ( $Code =~ m/^sub\s+RequestMethods[^}]+(get|head)[^}]+\}/xms && $Code =~ m{^sub\s+ValidationJSONBodyFields}xms )
+    {
+        die __PACKAGE__ . "\n" . <<EOF;
+Endpoints using the HEAD or GET request methods may not use a body payload. Use query params instead.
+EOF
+    }
+
+    return;
+}
+
+1;
