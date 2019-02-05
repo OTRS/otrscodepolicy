269e193be0ac6afea71b7f4698a294917a5eea8c
Martin Gruner <martin.gruner@otrs.com>
2019-02-05 11:07:08 +0100

    Added filter for evil HTML tags in framework/package PO files.

---
+++ Kernel/TidyAll/Plugin/OTRS/PO/HTMLTags.pm
@@ -0,0 +1,82 @@
+# --
+# Copyright (C) 2001-2019 OTRS AG, https://otrs.com/
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file COPYING for license information (GPL). If you
+# did not receive this file, see https://www.gnu.org/licenses/gpl-3.0.txt.
+# --
+
+package TidyAll::Plugin::OTRS::PO::HTMLTags;
+
+#
+# Filter forbidden HTML tags in Framework/Package translation files.
+#
+
+use strict;
+use warnings;
+
+use parent qw(TidyAll::Plugin::OTRS::Base);
+
+use Locale::PO ();
+
+sub validate_file {    ## no critic
+    my ( $Self, $Filename ) = @_;
+
+    return if $Self->IsPluginDisabled( Filename => $Filename );
+    return if $Self->IsFrameworkVersionLessThan( 4, 0 );
+
+    my $IsDocbookTranslation = $Filename =~ m{/doc-}smx;
+    return if $IsDocbookTranslation;
+
+    my @ForbiddenTags = (
+        qr(^<script)ixms,
+        qr(^<style)ixms,
+        qr(^<applet)ixms,
+        qr(^<object)ixms,
+        qr(^<svg)ixms,
+        qr(^<embed)ixms,
+        qr(^<meta)ixms,
+        qr(^<img)ixms,
+        qr(^<video)ixms,
+    );
+
+    my $Strings = Locale::PO->load_file_asarray($Filename);
+
+    my $ErrorMessage;
+
+    STRING:
+    for my $String ( @{ $Strings // [] } ) {
+        next STRING if $String->fuzzy();
+
+        my $Source      = $String->dequote( $String->msgid() )  // '';
+        my $Translation = $String->dequote( $String->msgstr() ) // '';
+
+        next STRING if !$Source && !$Translation;
+
+        my @InvalidTags;
+
+        for my $Part ( $Source, $Translation ) {
+            my @Tags = $Part =~ m{<[^>]*>}smg;
+
+            TAG:
+            for my $Tag (@Tags) {
+                for my $ForbiddenTag (@ForbiddenTags) {
+                    push @InvalidTags, @Tags if $Tag =~ $ForbiddenTag;
+                }
+            }
+        }
+
+        next STRING if !@InvalidTags;
+
+        $ErrorMessage .= "Invalid HTML tags found in line: " . $String->loaded_line_number() . "\n";
+        $ErrorMessage .= "  Source: $Source\n";
+        $ErrorMessage .= "  Translation: $Translation\n";
+        $ErrorMessage .= "  Invalid tags: @InvalidTags";
+    }
+
+    if ($ErrorMessage) {
+        die __PACKAGE__ . "\n$ErrorMessage";
+    }
+}
+
+1;
---
+++ scripts/test/OTRSCodePolicyPlugins/PO/HTMLTags.t
@@ -0,0 +1,65 @@
+# --
+# Copyright (C) 2001-2019 OTRS AG, https://otrs.com/
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file COPYING for license information (GPL). If you
+# did not receive this file, see https://www.gnu.org/licenses/gpl-3.0.txt.
+# --
+use strict;
+use warnings;
+
+use vars (qw($Self));
+use utf8;
+
+use scripts::test::OTRSCodePolicyPlugins;
+
+my @Tests = (
+    {
+        Name      => 'PO::HTMLTags, valid bold tag',
+        Filename  => 'otrs.de.po',
+        Plugins   => [qw(TidyAll::Plugin::OTRS::PO::HTMLTags)],
+        Framework => '6.0',
+        Source    => <<'EOF',
+msgid "String with <b>tag</b>"
+msgstr "Zeichenkette mit <b>Tag</b>"
+EOF
+        Exception => 0,
+    },
+    {
+        Name      => 'PO::HTMLTags, forbidden script tag',
+        Filename  => 'otrs.de.po',
+        Plugins   => [qw(TidyAll::Plugin::OTRS::PO::HTMLTags)],
+        Framework => '6.0',
+        Source    => <<'EOF',
+msgid "String with <sCrIpT>evil tag</script>"
+msgstr "Zeichenkette mit <script>bösem Tag</script>"
+EOF
+        Exception => 1,
+    },
+    {
+        Name      => 'PO::HTMLTags, valid paragraph tag',
+        Filename  => 'otrs.pot',
+        Plugins   => [qw(TidyAll::Plugin::OTRS::PO::HTMLTags)],
+        Framework => '6.0',
+        Source    => <<'EOF',
+msgid "<p>Paragraph string</p>"
+msgstr ""
+EOF
+        Exception => 0,
+    },
+    {
+        Name      => 'PO::HTMLTags, forbidden meta tag',
+        Filename  => 'otrs.pot',
+        Plugins   => [qw(TidyAll::Plugin::OTRS::PO::HTMLTags)],
+        Framework => '6.0',
+        Source    => <<'EOF',
+msgid "Redirecting now... <META http-equiv=\"refresh\" content=\"0; url=http://example.com/\">"
+msgstr ""
+EOF
+        Exception => 1,
+    },
+);
+
+$Self->scripts::test::OTRSCodePolicyPlugins::Run( Tests => \@Tests );
+
+1;
