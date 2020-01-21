1885b6256e017f902b5f4018aeb1422f909c73ef
Martin Gruner <martin.gruner@otrs.com>
2020-01-21 15:47:45 +0100

    Re-added needed nofilter.

--- Kernel/TidyAll/Plugin/OTRS/Migrations/OTRS6/PermissionDataNotInSession.pm
+++ Kernel/TidyAll/Plugin/OTRS/Migrations/OTRS6/PermissionDataNotInSession.pm
@@ -14,6 +14,7 @@ use warnings;
 use parent qw(TidyAll::Plugin::OTRS::Base);
 
 ## nofilter(TidyAll::Plugin::OTRS::Migrations::OTRS6::PermissionDataNotInSession)
+## nofilter(TidyAll::Plugin::OTRS::Perl::LayoutObject)
 ## nofilter(TidyAll::Plugin::OTRS::Perl::ObjectDependencies)
 
 sub validate_source {    ## no critic
