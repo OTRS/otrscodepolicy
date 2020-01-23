0b428e22c92c1d2b221b74b2029deb5d56bdeab3
Martin Gruner <martin.gruner@otrs.com>
2020-01-23 11:31:22 +0100

    Improved Perl::Critic policy.

--- Kernel/TidyAll/Plugin/OTRS/Perl/Critic/Policy/OTRS/RequireTrueReturnValueForModules.pm
+++ Kernel/TidyAll/Plugin/OTRS/Perl/Critic/Policy/OTRS/RequireTrueReturnValueForModules.pm
@@ -22,40 +22,28 @@ use Readonly;
 
 our $VERSION = '0.02';
 
-Readonly::Scalar my $DESC => q{Modules have to return a true value ("1;")};
-Readonly::Scalar my $EXPL => q{Use "1;" as the last statement of the module};
+Readonly::Scalar my $DESC => q{Modules and tests have to return a true value ("1;")};
+Readonly::Scalar my $EXPL => q{Use "1;" as the last statement of the file};
 
 sub supported_parameters { return; }
 sub default_severity     { return $SEVERITY_HIGHEST; }
 sub default_themes       { return qw( otrs ) }
 sub applies_to           { return 'PPI::Document' }
 
+# Only apply to Perl modules and test files, not to scripts.
+sub prepare_to_scan_document {
+    my ( $self, $document ) = @_;
+
+    return $document->logical_filename() =~ m{ (\.pm|\.t) \z }xms;
+}
+
 sub violates {
     my ( $self, $elem ) = @_;
 
-    return if $self->_is_script($elem);
-    return if $self->_returns_1($elem);
+    my $last_statement = $elem->schild(-1);
+    return if $last_statement && $last_statement eq '1;';
+
     return $self->violation( $DESC, $EXPL, $elem );
-}
-
-sub _returns_1 {
-    my ( $self, $elem ) = @_;
-
-    my $last_statement = ( grep { ref $_ eq 'PPI::Statement' } $elem->schildren() )[-1];
-    return 0 if !$last_statement;
-    return 1 if $last_statement eq '1;';
-    return 0;
-}
-
-sub _is_script {
-    my ( $self, $elem ) = @_;
-
-    my $document = $elem->document();
-    my $filename = $document->logical_filename();
-
-    my $is_module = $filename =~ m{ \.pm \z }xms;
-
-    return !$is_module;
 }
 
 1;
